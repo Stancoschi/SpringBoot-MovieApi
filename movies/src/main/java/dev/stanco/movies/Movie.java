624f09420c922da60eed71e3a7049b656587423a
Stancoschi <stancoschieduardrobert@gmail.com>
2024-08-12 15:23:27 +0300

    First Commit

---
+++ movies/src/main/java/dev/stanco/movies/Movie.java
@@ -0,0 +1,33 @@
+package dev.stanco.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+//Each document in the Movies Folder
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backDrops;
+    @DocumentReference
+    private List<Review> reviewIds;
+
+
+}
